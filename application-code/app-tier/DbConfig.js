8d9c733d6f861f063e6e51584ea9336e2eda79e7
kapilbharne <110342203+kapilbharne@users.noreply.github.com>
2023-08-13 16:46:48 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     DB_HOST : 'database-1-instance-1.cnehs048micx.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'Pass1234',
-    DB_DATABASE : 'database-1'
+    DB_DATABASE : 'webappdb'
 });
